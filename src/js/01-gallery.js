b4a3d017cb729c71784d4e0f5b4e8ef2d66e576c
olhazolotar <olchere@gmail.com>
2022-06-27 22:04:35 +0100

    add homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsEl = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li><a class="gallery__item" href=${original}>
+  <img class="gallery__image"
+  src=${preview}
+  alt=${description} />
+</a></li>`,
+  )
+  .join('');
+
+galleryItemsEl.innerHTML = markup;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,61 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+const dataForm = {
+    email: '',
+    message: '',
+}
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateInput();
+
+
+function onFormInput(event) {
+    // берем value input 
+    dataForm[event.target.name] = event.target.value;
+    // console.log(dataForm);
+
+    // делаем из обьекта строку
+    const dataFormJson = JSON.stringify(dataForm);
+
+    // записываем строку в localStorage
+    localStorage.setItem(STORAGE_KEY, dataFormJson);
+}
+
+ 
+function onFormSubmit(event) {  
+    // что б не перезагружалась страница при отправке 
+    event.preventDefault();
+
+    // очищает форму
+    event.currentTarget.reset(); 
+    
+    // при отправке очищает localStorage
+    localStorage.removeItem(STORAGE_KEY); 
+
+    //выводи объект с полями email, message и текущими их значениями в консоль
+    console.log(dataForm);
+}
+
+
+// берем данные из localStorage и заполняем им импут
+function populateInput() {
+    // берем данные из localStorage
+    const saveDataForm = localStorage.getItem(STORAGE_KEY);
+    
+    if (saveDataForm) {
+        const saveDataFormJson = JSON.parse(saveDataForm);
+        refs.email.value = saveDataFormJson.email;
+        refs.textarea.value = saveDataFormJson.message;      
+    };
+    
+};
